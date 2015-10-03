e68de3df055af0ba2c2712b2da6aeea978edd492
AJBucur <alexandrebucur@gmail.com>
2015-10-03 14:04:36 -0400

    Basic Panel Functionality
    Can look and interact with panel

---
+++ Assets/Scripts/PanelOperator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelOperator : MonoBehaviour {
+
+    Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                gameObject.GetComponent<Renderer>().material.color = Color.black;
+                
+            }
+        }
+	}
+}
