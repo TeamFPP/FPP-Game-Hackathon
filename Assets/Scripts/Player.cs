b525fb112110195afae1b6e8da32a5e7cba1b084
drkdagron <ste.mckillop@gmail.com>
2015-10-03 14:03:32 -0400

    Added Player Prefab with ray casting to each player looking for collisions hitting other players

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Material OpaqueMat;
+    public Material TransperentMat;
+    public GameObject helmetScreen;
+   
+    public void ChangeView(bool caught)
+    {
+        if (caught)
+            helmetScreen.GetComponent<MeshRenderer>().material = OpaqueMat;
+        else
+            helmetScreen.GetComponent<MeshRenderer>().material = TransperentMat;
+    }
+}
